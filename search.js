0745acc517922bff551578952d3a296776a048c7
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2019-01-11 15:12:47 -0600

    linear and binary solutions

--- search.js
+++ search.js
@@ -2,12 +2,30 @@
 const search = {
 
   linear: function (n, arr) {
-    // write code here
+    for ( let i = 0; i < arr.length; i++) {
+      if ( arr[i] === n) return i;
+    } 
+    return -1;
   },
 
-  binary: function (arr, n) {
-    // write code here
+  binary: function (arr, n, i = 0) {
+	
+    let mid = Math.ceil(arr.length / 2 -1 )
+    
+    if (arr[mid] === n) {
+      i += mid
+      return i;
 
+    } else if (arr[mid] > n) {
+      return this.binary(arr.slice(0, mid+1), n, i);
+
+    } else if (arr[mid] < n) {
+      i += mid + 1
+      return this.binary(arr.slice(mid+1, arr.length), n, i);
+
+    } 
+    return -1
+  
   }
 
 }
@@ -15,4 +33,3 @@ const search = {
 
 
 module.exports = search;
-
